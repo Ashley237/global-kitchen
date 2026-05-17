36cf4d0e2d3cc826bb6b8eb64446f8d59fa15ec6
Ngulefeh Ashley <ngulefehashley@gmail.com>
2026-05-17 09:37:21 +0100

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const recipeRoutes = require("./routes/recipeRoutes");
+
+mongoose
+  .connect("mongodb://localhost:27017/globalKitchenDB")
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.use(recipeRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Global Kitchen API is running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
